f0a62da7cbb3e6019215dcd984fe25c40e24a4a6
Roman Valenta <rvalenta4@gmail.com>
2020-04-19 09:55:55 +0200

    Remove useless import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { PostsModule } from './posts/posts.module'
 import { UsersModule } from './users/users.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { Connection } from 'typeorm'
 
 @Module({
 	imports: [TypeOrmModule.forRoot(), PostsModule, UsersModule]
